1092324fbb98d1d9dd49c0267f85eedb97672d63
Peter <pedromoyano454@gmail.com>
2023-08-25 14:41:40 -0300

    change to concepts and explanation

--- Nicholas-App/app.py
+++ Nicholas-App/app.py
@@ -15,39 +15,40 @@ from langchain.chains import LLMChain, SequentialChain
 
 # load .env file
 load_dotenv()
+
 #make api_key available to other moduless
 apikey = os.getenv('OPENAI_API_KEY')
-os.environ['OPENAI_API_KEY'] = apikey
-print(apikey)
 
 #App framework
 st.title("🦜️🔗 Peter's first langchain App")
-prompt = st.text_input("Enter a topic to write about:")
+prompt = st.text_input("Enter a topic you want to learn about:")
 
 
 #Prompt templates
 title_template = PromptTemplate(
     #A list of the names of the variables the prompt template expects.
     input_variables=['topic'],
-    template='Write me a youtube video title about {topic}.',
+    template='Make a list with the 5 most important concepts to grasp about {topic}. The list should only contain the concepts, not the explanation.',
 )
 
 script_template = PromptTemplate(
     #A list of the names of the variables the prompt template expects.
-    input_variables=['title'],
-    template='Write me a youtube video script based on this TITLE: {title}.',
+    input_variables=['concepts'],
+    template='explain the following concepts: {concepts}.',
 )
 
 
 #LLMs: Create an instance of the OpenAI server
 llm = OpenAI(model_name='gpt-3.5-turbo', temperature=0.9)
-##Create chain to run queries against LLMs. 'llm' parameter is required (LLM you want to call).
-title_chain = LLMChain(llm=llm, prompt=title_template, verbose=True, output_key='title')
-script_chain= LLMChain(llm=llm, prompt=script_template, verbose=True, output_key='script')
-sequential_chain = SequentialChain(chains=[title_chain, script_chain], input_variables=['topic'], output_variables=['title', 'script'], verbose=True)
+
+#Create chain to run queries against LLMs. 'llm' parameter is required (LLM you want to call).
+title_chain = LLMChain(llm=llm, prompt=title_template, verbose=True, output_key='concepts')
+script_chain= LLMChain(llm=llm, prompt=script_template, verbose=True, output_key='explanation')
+#The output of the first chain is the input of the second chain
+sequential_chain = SequentialChain(chains=[title_chain, script_chain], input_variables=['topic'], output_variables=['concepts', 'explanation'], verbose=True) 
 
 #Show the response to the screen if the user has entered a prompt
 if prompt: 
     response = sequential_chain({'topic': prompt})
-    st.write(response['title'])
-    st.write(response['script'])
+    st.write(response['concepts'])
+    st.write(response['explanation'])
