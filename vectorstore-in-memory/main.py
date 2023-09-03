ddaa7c286e3b1806b7b9924d029fd47b7fbed59d
Peter <pedromoyano454@gmail.com>
2023-09-03 05:09:46 -0300

    PDF reader and Chat bot implementatipon

--- vectorstore-in-memory/main.py
+++ vectorstore-in-memory/main.py
@@ -1,2 +1,25 @@
+import os
+from langchain.document_loaders import PyPDFLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chains import RetrievalQA
+from langchain.llms import OpenAI
+
 if __name__ == '__main__':
-    print 'Hello, world!
+    pdf_path = '/home/peter/LangChain/LangChain-Lab/vectorstore-in-memory/attention-is-all-you-need.pdf'
+    loader = PyPDFLoader(file_path=pdf_path)
+    documents = loader.load()
+    text_splitter = CharacterTextSplitter(
+        chunk_size=1000, chunk_overlap=30, separator='/n')
+    docs = text_splitter.split_documents(documents=documents)
+
+    embeddings = OpenAIEmbeddings()
+    vectorstore = FAISS.from_documents(docs, embeddings)
+    vectorstore.save_local('faiss_index_ReAct')
+
+    new_vectorstore = FAISS.load_local('faiss_index_ReAct', embeddings)
+    qa = RetrievalQA.from_chain_type(
+        llm=OpenAI(), chain_type="stuff", retriever=new_vectorstore.as_retriever())
+    res = qa.run(query="Explain ReAct, what problem does it solve?")
+    print(res)
